20430a0843ee142cca4100620505128def0a992d
Rodrigo Bossini <professorbossini@gmail.com>
2023-03-29 18:14:42 -0300

    apostila03(mss): fim do codigo 4.3.4

--- lembretes/index.js
+++ lembretes/index.js
@@ -1,15 +1,41 @@
+require('dotenv').config()
 const express = require ('express')
 const app = express()
+app.use(express.json())
 
+const lembretes = {}
+let idAtual = 0
 //GET localhost:4000/lembretes
 app.get('/lembretes', (req, res) => {
-
+  res.json(lembretes)
 })
 
 //POST localhost:4000/lembretes
+// {"texto": "Fazer café"}
+/*
+{
+  "1": {
+    "id": 1,
+    "texto": "Café"
+  },
+  "2": {
+    "id": 2,
+    "texto": "Cinema"
+  }
+}
+*/
 app.post('/lembretes', (req, res) => {
-
+  idAtual++;
+  const { texto } = req.body
+  lembretes[idAtual] = {
+    id: idAtual, texto
+  }
+  res.status(201).send(lembretes[idAtual])
 })
 
-app.listen(4000, () => console.log('Lembretes. Porta 4000.'))
+const { MSS_LEMBRETES_PORTA } = process.env
+app.listen(MSS_LEMBRETES_PORTA, () => {
+  console.log(`Lembretes. Porta ${MSS_LEMBRETES_PORTA}.`)
+})
+
 
